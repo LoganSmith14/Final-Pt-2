76ccdcc2188e42c8b3d3ee937e3c332641d9057d
LoganSmith14 <lpsmith06>
2016-05-03 08:27:55 -0400

    Added More Menu
    Finished off two options for the menu.

--- Source.cpp
+++ Source.cpp
@@ -6,21 +6,47 @@ int selection;
 
 int main(){
 	cout << "Logan Smith 1st Hour" << endl;
-	
-	cout << "**************************************" << endl;
-	cout << "* 1) Maze Game                       *" << endl;
-	cout << "* 2) Fight a Monster                 *" << endl;
-	cout << "* 3) Maze Game and Fight Monsters    *" << endl;
-	cout << "* 4) Stuff                           *" << endl;
-	cout << "* 5) Exit                            *" << endl;
-	cout << "**************************************" << endl;
-	cin >> selection;
-	switch (selection){
-	case 1:
-		cout << ""
-	}
-		
-	
+	bool run = true;
+	do{
+		cout << "**************************************" << endl;
+		cout << "* 1) Maze Game                       *" << endl;
+		cout << "* 2) Fight a Monster                 *" << endl;
+		cout << "* 3) Maze Game and Fight Monsters    *" << endl;
+		cout << "* 4) Stuff                           *" << endl;
+		cout << "* 5) Exit                            *" << endl;
+		cout << "**************************************" << endl;
+		cin >> selection;
+		switch (selection){
+		case 1:
+			cout << "You have choosen to walk the maze." << endl;
+		case 2:
+			cout << "You have choosen to fight." << endl;
+		case 3:
+			cout << "You have choosen to run the maze while fighting monsters." << endl;
+		case 4:
+			cout << "Here are some photos for you to not enjoy." << endl;
+			cout << "***********************************************************" << endl;
+			cout << "*                                                         *" << endl;
+			cout << "*                             #                           *" << endl;
+			cout << "*                            ###                          *" << endl;
+			cout << "*                           #####                         *" << endl;
+			cout << "*                          #######                        *" << endl;
+			cout << "*                         #########                       *" << endl;
+			cout << "*                        ###########                      *" << endl;
+			cout << "*                       #############                     *" << endl;
+			cout << "*                      ###############                    *" << endl;
+			cout << "*                     #################                   *" << endl;
+			cout << "*                    ###################                  *" << endl;
+			cout << "*                                                         *" << endl;
+			cout << "***********************************************************" << endl;
+ 		case 5:
+			cout << "You are Exiting the game." << endl;
+			run = false;
+			break;
+		default:
+			cout << "The number you selected is not here. Please try again." << endl;
+		}
+	} while (run);
 	system("pause");
 	return 0;
 }
